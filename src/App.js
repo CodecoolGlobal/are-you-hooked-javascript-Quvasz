b3228d42f5d9ce239bdeb535da1d45fb5aaa30d4
Pap László <quvasz87@gmail.com>
2022-09-07 21:35:53 +0200

    Add solution

--- src/App.js
+++ src/App.js
@@ -3,6 +3,79 @@ import users from "./fake_data";
 
 const App = () => {
     const [filteredEmployees, setFilteredEmployees] = useState(users);
+    const [filterText, setFilterText] = useState("");
+    const [passwordMatchError, setPasswordMatchError] = useState(false);
+    const [validPasswordMatch, setValidPasswordMatch] = useState(false);
+    const [passwordComplexityError, setPasswordComplexityError] = useState(false);
+    const [complexityErrorText, setcomplexityErrorText] = useState("");
+    const [passwordIsComplex, setPasswordIscomplex] = useState(false);
+    const [passwordTexts, setPasswordTexts] = useState(["", ""]);
+    let usersTmp = users;
+
+    function filterEmployees() {
+        return usersTmp.filter(employee => employee.name.toLowerCase().includes(filterText));
+    }
+
+    function searchFieldInput(e) {
+        setFilterText(e.target.value.toLowerCase());
+        setFilteredEmployees(filterEmployees());
+    }
+
+    function increaseSalary(id) {
+        usersTmp = usersTmp.map(employee => {
+            if (employee.id === id) employee.salary += 100;
+            return employee;
+        })
+        setFilteredEmployees(filterEmployees());
+    }
+
+    function decreaseSalary(id) {
+        usersTmp = usersTmp.map(employee => {
+            if (employee.id === id) employee.salary -= 100;
+            return employee;
+        })
+        setFilteredEmployees(filterEmployees());
+    }
+
+    function passwordInput(text1, text2) {
+        const newTexts = passwordTexts;
+        if (text1 !== null) newTexts[0] = text1;
+        if (text2 !== null) newTexts[1] = text2;
+
+        setPasswordTexts(newTexts);
+    }
+
+    function checkPasswordsMatch() {
+        if (passwordTexts[0] !== "" && passwordTexts[1] !== "") {
+            setPasswordMatchError(passwordTexts[0] !== passwordTexts[1]);
+            setValidPasswordMatch(passwordTexts[0] === passwordTexts[1]);
+        }
+        else {
+            setPasswordMatchError(false);
+            setValidPasswordMatch(false);
+        }
+    }
+
+    function checkComplexity() {
+        let textResult = "Password must: be";
+        let complex = true;
+        let error = false;
+        const errorTexts = [];
+        if (passwordTexts[0].length < 8) errorTexts.push(" be at least 8 characters long");
+        if (passwordTexts[0].search(/[!@#$%^&*]/) === -1) errorTexts.push(" contain a special symbol");
+        if (passwordTexts[0].search(/[A-Z]/) === -1) errorTexts.push(" contain a capital letter");
+        if (passwordTexts[0].search(/[0-9]/) === -1) errorTexts.push(" contain a number");
+
+        if (errorTexts.length > 0) {
+            textResult += errorTexts.join(" &");
+            setcomplexityErrorText(textResult);
+            complex = false;
+            error = true;
+        }
+        setPasswordComplexityError(error);
+        setPasswordIscomplex(complex);
+    }
+
 
     return (
         <>
@@ -14,15 +87,26 @@ const App = () => {
 
             <section>
                 <div className="container">
-                    <p className="error">Passwords do not match</p>
-                    <p className="error">Password needs to be more complex</p>
+                    {passwordIsComplex === true && <p className="valid">Good choice of password!</p>}
+                    {validPasswordMatch === true && <p className="valid">Access granted</p>}
+                    {passwordMatchError === true && <p className="error">Passwords do not match</p>}
+                    {passwordComplexityError === true && <p className="error">{complexityErrorText}</p>}
                     <input
                         type="password"
                         placeholder="Enter your password"
+                        onInput={(e) => {
+                            passwordInput(e.target.value, null);
+                            checkPasswordsMatch();
+                            checkComplexity();
+                        }}
                     />
                     <input
                         type="password"
                         placeholder="Verify your password"
+                        onInput={(e) => {
+                            passwordInput(null, e.target.value);
+                            checkPasswordsMatch();
+                        }}
                     />
                 </div>
             </section>
@@ -35,8 +119,9 @@ const App = () => {
                                 <th>ID</th>
                                 <th>
                                     <input
-                                            type="text"
-                                            placeholder="Name..."
+                                        type="text"
+                                        placeholder="Name..."
+                                        onInput={searchFieldInput}
                                     />
                                 </th>
                                 <th>Age</th>
@@ -51,11 +136,11 @@ const App = () => {
                                         <td>{employee.name}</td>
                                         <td>{employee.age}</td>
                                         <td>
-                                            <button>
+                                            <button onClick={() => { decreaseSalary(employee.id) }}>
                                                 -
                                             </button>
                                             <span>{employee.salary}</span>
-                                            <button>
+                                            <button onClick={() => { increaseSalary(employee.id) }}>
                                                 +
                                             </button>
                                         </td>
